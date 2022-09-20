b3c79729a55cd4d533a212f8d2886a48dfc2d277
Aleksei Sheifler <asheifler@devexperts.com>
2022-09-20 18:34:08 +0200

    [EN-4934] Create POC framework. set-rpath for mac

--- src/test/java/com/dxfeed/NativeLibMain.java
+++ src/test/java/com/dxfeed/NativeLibMain.java
@@ -1,18 +1,18 @@
+package com.dxfeed;
+
 import com.dxfeed.api.DXEndpoint;
 import com.dxfeed.api.DXFeedSubscription;
 import com.dxfeed.event.market.MarketEvent;
 import com.dxfeed.event.market.Quote;
 import com.dxfeed.event.market.TimeAndSale;
 
-import java.io.IOException;
 import java.util.Arrays;
-import java.util.List;
 
 public class NativeLibMain {
     /**
      * The function name must be converted to run_main in native library.
      */
-    public static void main(final String[] args) throws InterruptedException, IOException {
+    public static void main(final String[] args) throws InterruptedException {
         final String token = System.getProperty("token");
         final DXEndpoint dxEndpoint = DXEndpoint.newBuilder()
                 .withRole(DXEndpoint.Role.FEED)
