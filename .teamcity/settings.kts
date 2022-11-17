cece05205b36e318e4e7ffedbed78904153d20f3
Aleksei Sheifler <asheifler@devexperts.com>
2022-11-17 11:25:27 +0100

    [EN-5686] [Multiplatform API (GraalVM)] CI/CD osx

---
+++ .teamcity/settings.kts
@@ -0,0 +1,60 @@
+import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.ui.add
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2021.2"
+
+project {
+
+    buildType(Build)
+}
+//dxcity: winAgent4450  host: winbuilder4450.in.devexperts.com
+//dxcity: dxAgent1707-1  host: dxbuilder1707.in.devexperts.com
+//dxcity: macbuilder10  host: macbuilder10.in.devexperts.com
+
+object Build : BuildType({
+    name = "Build"
+    description = "Build description"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        maven {
+            name = "mvn package"
+            goals = "clean package"
+            pomLocation = "pom.xml"
+        }
+    }
+
+    triggers {
+        vcs {
+            enabled = false
+        }
+    }
+})
---
+++ scripts/agent-macbuilder10.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+brew install jenv
+jenv enable-plugin maven
