2a369eace92cc7166242bbf3d937ffc8b3feb9b2
Aleksei Sheifler <asheifler@devexperts.com>
2022-12-13 16:45:21 +0100

    fix: DXEndpoint.getInstance().close() - close the singleton and do not generate a configuration for the latest cases

--- src/test/java/com/dxfeed/NativeLibMain.java
+++ src/test/java/com/dxfeed/NativeLibMain.java
@@ -27,59 +27,61 @@ public class NativeLibMain {
    * The function name must be converted to run_main in native library.
    */
   public static void main(final String[] args) throws InterruptedException {
-    {
-      final DXEndpoint dxEndpoint = DXEndpoint.getInstance();
-      dxEndpoint.connect("demo.dxfeed.com:7300");
-      final DXFeedSubscription<Order> subscription = dxEndpoint.getFeed()
-          .createSubscription(Order.class);
-      subscription.addEventListener(System.out::println);
-      subscription.addSymbols("AAPL");
-      final Promise<List<Series>> aapl = dxEndpoint.getFeed()
-          .getIndexedEventsPromise(Series.class, "AAPL", IndexedEventSource.DEFAULT);
-      aapl.awaitWithoutException(30, TimeUnit.SECONDS);
-      final List<Series> result = aapl.getResult();
-      final Throwable exception = aapl.getException();
-      dxEndpoint.close();
-    }
-    {
-      final String token = System.getProperty("token");
-      final DXEndpoint dxEndpoint = DXEndpoint.newBuilder()
-          .withRole(DXEndpoint.Role.FEED)
-          .withProperties(System.getProperties())
-          .build().user("demo").password("demo");
-      final DXFeedSubscription<MarketEvent> subscription = dxEndpoint.getFeed()
-          .createSubscription(Quote.class, TimeAndSale.class);
-      dxEndpoint.connect("lessona.dxfeed.com:7905[login=entitle:" + token + "]");
-      subscription.addEventListener(System.out::println);
-      subscription.addSymbols(
-          Arrays.asList("AAPL", "MSFT", "AMZN", "YHOO", "IBM", "SPX", "ETH/USD:GDAX", "EUR/USD",
-              "BTC/USDT:CXBINA"));
-      Thread.sleep(1000);
-      dxEndpoint.close();
-    }
-    {
-      final DXEndpoint dxEndpoint = DXEndpoint.getInstance();
-      dxEndpoint.connect("demo.dxfeed.com:7300");
-      DXFeedTimeSeriesSubscription<TimeSeriesEvent<?>> subscription = dxEndpoint.getFeed()
-          .createTimeSeriesSubscription(TimeAndSale.class, TheoPrice.class, Underlying.class,
-              Candle.class,
-              Greeks.class, DailyCandle.class);
-      subscription.setFromTime(0);
-      subscription.addEventListener(System.out::println);
-      subscription.addSymbols(
-          Arrays.asList("AAPL", "MSFT", "AMZN", "YHOO", "IBM", "SPX", "ETH/USD:GDAX", "EUR/USD",
-              "BTC/USDT:CXBINA"));
-      Thread.sleep(1000);
-      dxEndpoint.close();
-    }
-    {
-      final DXEndpoint dxEndpoint = DXEndpoint.getInstance();
-      dxEndpoint.connect("tape.csv[format=csv,readAs=ticker_data,cycle,speed=max]");
-      final var subscription = dxEndpoint.getFeed().createSubscription(Quote.class);
-      subscription.addEventListener(System.out::println);
-      subscription.addSymbols("AAPL");
-      Thread.sleep(1000);
-      dxEndpoint.close();
-    }
+
+    final DXEndpoint dxEndpointInstance = DXEndpoint.getInstance();
+    dxEndpointInstance.connect("demo.dxfeed.com:7300");
+    final DXFeedSubscription<Order> subscription = dxEndpointInstance.getFeed()
+        .createSubscription(Order.class);
+    subscription.addEventListener(System.out::println);
+    subscription.addSymbols("AAPL");
+    final Promise<List<Series>> aapl = dxEndpointInstance.getFeed()
+        .getIndexedEventsPromise(Series.class, "AAPL", IndexedEventSource.DEFAULT);
+    aapl.awaitWithoutException(30, TimeUnit.SECONDS);
+    final List<Series> result = aapl.getResult();
+    final Throwable exception = aapl.getException();
+    dxEndpointInstance.close();
+
+    final String token = System.getProperty("token");
+    final DXEndpoint dxEndpoint1 = DXEndpoint.newBuilder()
+        .withRole(DXEndpoint.Role.FEED)
+        .withProperties(System.getProperties())
+        .build().user("demo").password("demo");
+    final DXFeedSubscription<MarketEvent> subscription1 = dxEndpoint1.getFeed()
+        .createSubscription(Quote.class, TimeAndSale.class);
+    dxEndpoint1.connect("lessona.dxfeed.com:7905[login=entitle:" + token + "]");
+    subscription1.addEventListener(System.out::println);
+    subscription1.addSymbols(
+        Arrays.asList("AAPL", "MSFT", "AMZN", "YHOO", "IBM", "SPX", "ETH/USD:GDAX", "EUR/USD",
+            "BTC/USDT:CXBINA"));
+    Thread.sleep(1000);
+    dxEndpoint1.close();
+
+    final DXEndpoint dxEndpoint2 = DXEndpoint.newBuilder()
+        .withRole(DXEndpoint.Role.FEED)
+        .withProperties(System.getProperties())
+        .build().user("demo").password("demo");
+    dxEndpoint2.connect("demo.dxfeed.com:7300");
+    DXFeedTimeSeriesSubscription<TimeSeriesEvent<?>> subscription2 = dxEndpoint2.getFeed()
+        .createTimeSeriesSubscription(TimeAndSale.class, TheoPrice.class, Underlying.class,
+            Candle.class,
+            Greeks.class, DailyCandle.class);
+    subscription2.setFromTime(0);
+    subscription2.addEventListener(System.out::println);
+    subscription2.addSymbols(
+        Arrays.asList("AAPL", "MSFT", "AMZN", "YHOO", "IBM", "SPX", "ETH/USD:GDAX", "EUR/USD",
+            "BTC/USDT:CXBINA"));
+    Thread.sleep(1000);
+    dxEndpoint2.close();
+
+    final DXEndpoint dxEndpoint3 = DXEndpoint.newBuilder()
+        .withRole(DXEndpoint.Role.FEED)
+        .withProperties(System.getProperties())
+        .build().user("demo").password("demo");
+    dxEndpoint3.connect("tape.csv[format=csv,readAs=ticker_data,cycle,speed=max]");
+    final var subscription3 = dxEndpoint3.getFeed().createSubscription(Quote.class);
+    subscription3.addEventListener(System.out::println);
+    subscription3.addSymbols("AAPL");
+    Thread.sleep(1000);
+    dxEndpoint3.close();
   }
 }
