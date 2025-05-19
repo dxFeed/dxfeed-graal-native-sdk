95ff72467d8a2a48b7dcc4b06e88bf2a9a25d5d7
Anatoly Kalin <anatoly.kalin@devexperts.com>
2025-05-19 18:06:29 +0300

    Switch PowerShell step to generic script in build process
    Replaced the PowerShell-based build script with a platform-independent generic script step. Updated syntax to use TeamCity environment variable referencing format for better compatibility and maintainability. This change simplifies the CI pipeline and aligns with cross-platform standards.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -721,22 +721,20 @@ object BuildAndPushDockerImageForWindowsX64 : BuildType({
     }
 
     steps {
-        powerShell {
+        script {
             name = "Build"
-            scriptMode = script {
-                content = """
-                    cd .teamcity
-                    docker images --all
-                    docker rmi -f @(docker images -aq)
-                    docker images --all
-                    docker login dxfeed-docker.jfrog.io --username $env:JFROG_USER --password $env:JFROG_PASSWORD
-                    docker build -t "dxfeed-docker.jfrog.io/dxfeed-api/graalvm:win-x64-$env:GRAALVM_VERSION" --build-arg GRAALVM_VERSION="$env:GRAALVM_VERSION" -f graalvm-win-x64-v2.Dockerfile .
-                    docker push "dxfeed-docker.jfrog.io/dxfeed-api/graalvm:win-x64-$env:GRAALVM_VERSION"
-                    docker images --all
-                    docker rmi -f @(docker images -aq)
-                    docker logout
-                """.trimIndent()
-            }
+            scriptContent = """
+                cd .teamcity
+                docker images --all
+                docker rmi -f ${'$'}(docker images -aq)
+                docker images --all
+                docker login dxfeed-docker.jfrog.io --username %env.JFROG_USER% --password %env.JFROG_PASSWORD%
+                docker build -t dxfeed-docker.jfrog.io/dxfeed-api/graalvm:win-x64-%env.GRAALVM_VERSION% --build-arg GRAALVM_VERSION="%env.GRAALVM_VERSION%" -f graalvm-win-x64-v2.Dockerfile .
+                docker push dxfeed-docker.jfrog.io/dxfeed-api/graalvm:win-x64-%env.GRAALVM_VERSION%
+                docker images --all
+                docker rmi -f ${'$'}(docker images -aq)
+                docker logout
+            """.trimIndent()
             formatStderrAsError = true
         }
     }
