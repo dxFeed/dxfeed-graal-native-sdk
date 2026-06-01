f7ae869119028ea45f0435189dbadb6b061e7df3
yury.shcharbina <yury.shcharbina@TeamCity>
2026-06-01 23:40:28 +0000

    parameters of 'Copy service images' build configuration were updated (TeamCity change in 'dxFeed / Eugenics / Dxfeed Graal Native Api' project)

--- .teamcity/patches/buildTypes/CopyServiceImages.kts
+++ .teamcity/patches/buildTypes/CopyServiceImages.kts
@@ -19,7 +19,7 @@ create(DslContext.projectId, BuildType({
     params {
         password("env.jfrogPass", "credentialsJSON:d288798f-47b9-4fbb-8463-a68be694481d")
         param("env.target.repo", "nexus-docker-graalvm.in.devexperts.com")
-        param("env.srcRepo", "dxfeed-docker.jfrog.io/dxfeed-api/graalvm:linux-aarch64-jdk-23.0.2")
+        param("env.srcRepo", "dxfeed-docker.jfrog.io/dxfeed-api/graalvm:linux-x64-jdk-23.0.2")
     }
 
     vcs {
