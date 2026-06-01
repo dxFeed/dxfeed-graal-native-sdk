a735d61e17ad0ae2d374d01f4ad23275844b5fee
amordovskii <amordovskii@TeamCity>
2026-06-01 10:57:37 +0000

    runners of 'Copy service images' build configuration were updated (TeamCity change in 'dxFeed / Eugenics / Dxfeed Graal Native Api' project)

--- .teamcity/patches/buildTypes/CopyServiceImages.kts
+++ .teamcity/patches/buildTypes/CopyServiceImages.kts
@@ -25,8 +25,11 @@ create(DslContext.projectId, BuildType({
             name = "test"
             id = "test"
             scriptContent = """
+                echo "Installing Skopeo"
                 apt-get update
                 apt-get install -y skopeo
+                
+                skopeo copy --src-creds amordovskii:pass --dest-creds user:pass docker://src.reg/img docker://dest.reg/img
             """.trimIndent()
             dockerImage = "ubuntu:latest"
             dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
