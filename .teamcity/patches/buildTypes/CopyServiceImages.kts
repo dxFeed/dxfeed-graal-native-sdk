fa4b1b0d4e684beab516d7a26332050ca0668caa
amordovskii <amordovskii@TeamCity>
2026-06-01 11:38:50 +0000

    runners of 'Copy service images' build configuration were updated (TeamCity change in 'dxFeed / Eugenics / Dxfeed Graal Native Api' project)

--- .teamcity/patches/buildTypes/CopyServiceImages.kts
+++ .teamcity/patches/buildTypes/CopyServiceImages.kts
@@ -36,7 +36,7 @@ create(DslContext.projectId, BuildType({
                 apt-get install -y skopeo
                 apt-get install -y ca-certificates 
                 
-                image_tag=${'$'}(echo "%env.srcRepo%" | cut -d "/" -f 9)
+                image_tag=${'$'}(echo "%env.srcRepo%" | cut -d "/" -f 6)
                 echo "Image tag is ${'$'}image_tag"
                 echo "Source image - %env.srcRepo%:latest"
                 echo "Target image - %env.target.repo%/${'$'}image_tag"
